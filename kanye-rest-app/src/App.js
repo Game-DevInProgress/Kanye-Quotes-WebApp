4da3d4a3656a0b9282e167da7562260ca15c79d9
Game-DevInProgress <66549060+Game-DevInProgress@users.noreply.github.com>
2022-02-22 02:22:35 -0500

    Handled quote outside of App
    Made Quote.js to handle how to format the quotes that are in the array

--- kanye-rest-app/src/App.js
+++ kanye-rest-app/src/App.js
@@ -1,5 +1,18 @@
 import React,{useEffect, useState} from "react";
+import Quote from "./Quote";
 import './App.css';
+
+const images = [
+  "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_42/3513498/211019-ye-kanye-west-mb-1112.JPG",
+  "https://www.gannett-cdn.com/presto/2021/08/06/USAT/aea3c900-0159-4fd0-947e-6ad8d4e3d79e-AP_Music_Kanye_West.jpg?crop=2999,1687,x1,y0&width=2999&height=1687&format=pjpg&auto=webp",
+  "https://pagesix.com/wp-content/uploads/sites/3/2021/10/kanye-name-change.jpg?quality=80&strip=all&w=618&h=410&crop=1",
+  "https://www.gannett-cdn.com/presto/2020/06/07/USAT/99b83fac-f9e2-469c-9be1-39ce22f6caba-25426.jpg",
+  "https://static.independent.co.uk/2021/12/16/11/newFile-9.jpg?quality=75&width=1200&auto=webp",
+  "https://e3.365dm.com/21/08/2048x1152/2372108290423928107_5494583.jpg",
+  "https://www.hellomagazine.com/imagenes/celebrities/20210825120425/kanye-west-legally-changes-name-ye/0-581-941/kanyewest-t.jpg",
+  "https://i.pinimg.com/originals/df/25/96/df2596b086219446585c8c78e6a94a75.jpg",
+  "https://www.mandatory.com/assets/uploads/2022/02/GettyImages-1366439992-e1644795337767.jpg"
+]
 
 const App = () => {
 
@@ -23,7 +36,7 @@ const App = () => {
   return (
     <div className="App">
       {quotes.map(quote => (
-        <p>"{quote}"</p>
+        <Quote saying={quote}/>
       ))}
     </div>
   );
---
+++ kanye-rest-app/src/Quote.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Quote = ({saying}) => {
+    return(
+        <div>
+            <img src="" alt="Picture Source Not Found"/>
+            <h1>{saying}</h1>
+        </div>
+    );
+};
+
+export default Quote;
